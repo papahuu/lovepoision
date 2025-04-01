c72797817592dbcece778f51e589349ddc5f369f
MisteryDevs <officialbotdev@gmail.com>
2025-04-01 17:49:16 +0530

    Update queue.py

--- AviaxMusic/utils/inline/queue.py
+++ AviaxMusic/utils/inline/queue.py
@@ -65,6 +65,17 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
+        [   
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/RADHE_MUSIC_ROBOT?startgroup=true&admin=delete_messages+invite_users",)
+        ],
+        [
+            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [
             InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
