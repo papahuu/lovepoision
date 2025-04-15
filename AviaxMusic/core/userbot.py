6f9936fdda075c71af49e99343b3d81b283fe222
TheRadhe <rajveer202415@gmail.com>
2025-04-16 00:14:34 +0530

    Update userbot.py

--- AviaxMusic/core/userbot.py
+++ AviaxMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AviaxOfficial")
-                await self.one.join_chat("AviaxSupport")
+                await self.one.join_chat("ll_BOTCHAMBER_ll")
+                await self.one.join_chat("DP_WORLD7")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ur_rishu_143")
-                await self.one.join_chat("nenobots")
+                await self.two.join_chat("DP_WORLD7")
+                await self.one.join_chat("ll_BOTCHAMBER_ll")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("nenobots")
-                await self.one.join_chat("ur_rishu_143")
+                await self.three.join_chat("ll_BOTCHAMBER_ll")
+                await self.one.join_chat("DP_WORLD7")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ur_rishu_143")
-                await self.one.join_chat("nenobots")
+                await self.four.join_chat("DP_WORLD7")
+                await self.one.join_chat("ll_BOTCHAMBER_ll")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("nenobots")
-                await self.one.join_chat("ur_rishu_143")
+                await self.five.join_chat("ll_BOTCHAMBER_ll")
+                await self.one.join_chat("DP_WORLD7")
             except:
                 pass
             assistants.append(5)
